4d1a459393c754891a1fafc51ebe6adfc81cc243
MohamedFathi <mohamedkdr66@yahoo.com>
2023-06-18 01:29:12 +0300

    add services migrations and add ERD File

--- services/models.py
+++ services/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+    icon = models.ImageField(upload_to ='media/services')
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "services"
+
+class ServiceTranslation(models.Model):
+    locale = models.CharField(max_length=2)
+    service = models.ForeignKey(Service,on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True)
+
+    class Meta:
+        db_table = "service_translations"
